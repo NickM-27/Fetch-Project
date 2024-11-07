eead04a9c70dbcd147e5efe3d1e644a4f6bf09ef
Nicolas Mowen <nickmowen213@gmail.com>
2024-11-07 08:53:26 -0700

    Add ability to toggle display between list and grid

--- app/src/main/java/com/nick/mowen/fetch/screens/ListingScreen.kt
+++ app/src/main/java/com/nick/mowen/fetch/screens/ListingScreen.kt
@@ -5,10 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +23,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -29,11 +38,18 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import java.util.SortedMap
 
+enum class ListMode {
+    LIST,
+    GRID
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListingScreen(listingsFlow: StateFlow<SortedMap<Int, List<ListingData>>>) {
     val listings = listingsFlow.collectAsState()
     val scrollState = rememberScrollState()
+
+    val listMode = remember { mutableStateOf(ListMode.LIST) }
 
     Scaffold(
         topBar = {
@@ -41,7 +57,19 @@ fun ListingScreen(listingsFlow: StateFlow<SortedMap<Int, List<ListingData>>>) {
                 title = { Text(stringResource(R.string.title_fetch_project)) },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer
-                )
+                ),
+                actions = {
+                    IconButton(
+                        onClick = {
+                            listMode.value = if (listMode.value == ListMode.LIST) ListMode.GRID else ListMode.LIST
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.List,
+                            contentDescription = "Toggle list mode"
+                        )
+                    }
+                }
             )
         }
     ) { padding ->
@@ -53,21 +81,37 @@ fun ListingScreen(listingsFlow: StateFlow<SortedMap<Int, List<ListingData>>>) {
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             listings.value.forEach { (id, items) ->
-                ListingRow(id, items)
+                ListingRow(id, items, listMode.value)
             }
         }
     }
 }
 
 @Composable
-fun ListingRow(id: Int, items: List<ListingData>) {
+fun ListingRow(id: Int, items: List<ListingData>, mode: ListMode) {
     val scrollState = rememberScrollState()
 
     Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
         Text(stringResource(R.string.template_listing, id), fontWeight = FontWeight.Bold)
-        Row(modifier = Modifier.horizontalScroll(scrollState), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items.forEach { item ->
-                ListingCard(item)
+
+        if (mode == ListMode.LIST) {
+            Row(modifier = Modifier.horizontalScroll(scrollState), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                items.forEach { item ->
+                    ListingCard(item)
+                }
+            }
+        } else {
+            LazyVerticalGrid(
+                modifier = Modifier.size(800.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                columns = GridCells.Fixed(4)
+            ) {
+                items.forEach { item ->
+                    item {
+                        ListingCard(item)
+                    }
+                }
             }
         }
     }
