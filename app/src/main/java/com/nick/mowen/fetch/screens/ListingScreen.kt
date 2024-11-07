c38dc47a98d600995c760ea7a66da532d6863b5d
Nicolas Mowen <nickmowen213@gmail.com>
2024-11-07 08:42:18 -0700

    Remove appbar and use compose

--- app/src/main/java/com/nick/mowen/fetch/screens/ListingScreen.kt
+++ app/src/main/java/com/nick/mowen/fetch/screens/ListingScreen.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -25,12 +29,22 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import java.util.SortedMap
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListingScreen(listingsFlow: StateFlow<SortedMap<Int, List<ListingData>>>) {
     val listings = listingsFlow.collectAsState()
     val scrollState = rememberScrollState()
 
-    Scaffold { padding ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.title_fetch_project)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
+    ) { padding ->
         Column(
             modifier = Modifier
                 .padding(padding)
